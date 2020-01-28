b20e589699a8883526bccb3ed4d354d9a910e80c
nelevsahm <58182984+nelevsahm@users.noreply.github.com>
2020-01-28 21:36:10 +0100

    Update schraeg.asm

--- schraeg.asm
+++ schraeg.asm
@@ -2,7 +2,6 @@
 
 %define SYS_WRITE	4
 %define SYS_EXIT	1
-
 %define STDOUT		1
 
 section .data
@@ -11,14 +10,14 @@ section .text
 	global _start
 _start:
 ;pop Anzahl der Argumente inklusive Programmname
-	pop eax
+	pop edi
 ;pop Programmname
 	pop esi
 
 check_arguments:
-mov esi, 0
+xor esi, esi
 ;ist Anzahl der Argumente gleich 1
-	cmp eax, 1
+	cmp edi, 1
 ;wenn nicht groesser als 1 beende programm
 	jle end_program
 	pop ebx
@@ -31,13 +30,18 @@ print_letter:
 	push edx
 	
 	inc esi
-	mov eax, SYS_WRITE
-;;mov ecx buchstabe, was du schreiben willst, +1 für kommende
-	mov ecx, ebx
+	mov edx, 1		;;edx 1, 1 Buchstabe
+	mov ecx, ebx		;;mov ecx buchstabe, was du schreiben willst, +1 für kommende
 	mov ebx, STDOUT
-;;edx 1, 1 buchstabe
-	mov edx, 1
+	mov eax, SYS_WRITE
 
+	int 80h
+	mov eax, SYS_WRITE       
+    	mov ebx, STDOUT          
+    	mov ecx, newline         
+    	mov edx, 1
+	
+	dec edi
 
 	pop edx
 	pop ecx
